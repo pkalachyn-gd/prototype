064255a8479ad57e38f1b08c1c6564ee9215f331
Piotr Kalachyn <piotr.kalachyn@sienn.pl>
2022-05-10 00:31:25 +0200

    catching again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,20 @@ import {
   ViewChildren,
 } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+interface MenuItem {
+  label: string;
+  routerLink: string;
+  isButton: Boolean;
+}
+
+const MENU_ITEMS: MenuItem[] = [
+  { label: 'Home', routerLink: '/', isButton: false },
+  { label: 'Prototype', routerLink: 'prototype', isButton: true },
+  { label: 'Dashboards', routerLink: 'dashboards', isButton: true },
+];
 
 @Component({
   selector: 'app-root',
@@ -16,12 +30,17 @@ import { RouterLink } from '@angular/router';
 export class AppComponent implements OnDestroy, AfterViewInit {
   @ViewChildren(RouterLink) menuButtons!: QueryList<HTMLElement>;
 
+  menuItems$ = of(MENU_ITEMS).pipe(delay(6666));
   constructor(private focusMonitor: FocusMonitor) {}
 
   ngAfterViewInit() {
-    this.menuButtons.forEach((menuButton) =>
-      this.focusMonitor.monitor(menuButton)
-    );
+    console.info(this.menuButtons);
+    this.menuButtons.changes.subscribe((links: HTMLElement[]) => {
+      console.info(links);
+      links.forEach((menuButton) =>
+        this.focusMonitor.monitor(menuButton).subscribe(console.log)
+      );
+    });
   }
 
   ngOnDestroy() {
